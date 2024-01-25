7da3c582acac186d63981fe039b1ce1fa39fb0e6
nicat.guliyev <saatovcavid.work@gmail.com>
2024-01-25 14:41:56 +0400

    2 textfields added to Login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:social_media/pages/login_page.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -8,9 +10,10 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return const MaterialApp(
       title: 'Flutter Demo',
-      home: MyHomePage(),
+      home: LoginPage(),
     );
   }
 }
